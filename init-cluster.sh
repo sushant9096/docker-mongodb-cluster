ec18da7f01b9676efede989454cf760e68eda721
Sushant Zope <sushantzope9096@gmail.com>
2022-12-10 05:52:29 +0000

    Fix init-cluster shell commands

--- init-cluster.sh
+++ init-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-openssl rand -base64 756 > security.key
-sudo chmod 400 security.key
+sudo openssl rand -base64 756 > security.key
+sudo chmod 0400 ./security.key
 docker compose up -d
 
 sleep 5
